df57860a35f7479a288f527787d78296fd6d2f41
mursel <murselmusabasic@gmail.com>
2011-12-15 15:44:04 +0100

    new file:   fileio.js

---
+++ fileio.js
@@ -0,0 +1,19 @@
+﻿var fs = require('fs');
+
+var fileStream = undefined;
+
+exports.init = function (fileName, append, enc, fn) {
+    encoding = (enc == undefined) ? 'utf8' : enc;
+    if (append) {
+        fileStream = fs.createWriteStream(fileName, { 'flags': 'a', 'encoding': '" + encoding+"' });
+    } else {
+        fileStream = fs.createWriteStream(fileName, { 'encoding': '" + encoding+"' });
+    }
+
+    fn("File created!");
+}
+
+exports.write = function (msg) {
+    fileStream.write(msg);
+}
+
